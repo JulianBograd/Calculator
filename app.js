10cc15a149a5190221f277e7eea83ab1edb623d5
JulianBograd <bogradjuli@gmail.com>
2022-09-14 17:14:56 -0300

    Terminado el diseño css - agregado de archivo js

---
+++ app.js
@@ -0,0 +1,7 @@
+let currentInput = document.querySelector('.currentInput');
+let answerScreen = document.querySelector('.answerScreen');
+let buttons = document.querySelectorAll('button');
+let erasebtn = document.querySelector('#erase');
+let clearbtn = document.querySelector('#clear');
+let evaluate = document.querySelector('#evaluate');
+let realTimeScreenValue = []
